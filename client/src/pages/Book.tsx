7b393993fd54ebd21f77212b6dd1669125b51cfc
holygangesvns <holygangesvns@gmail.com>
2026-05-29 17:26:08 +0530

    Update Book.tsx

--- client/src/pages/Book.tsx
+++ client/src/pages/Book.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import Navbar from "@/components/Navbar";
 import { Star } from "lucide-react";
 
 export default function Book() {
@@ -20,8 +19,6 @@ export default function Book() {
   // --- END NOBEDS RESIZE LOGIC ---
 
   return (
-    <>
-      <Navbar />
       <div className="min-h-screen bg-gray-50 pt-20">
         <div className="container mx-auto px-4 py-12">
           <div className="max-w-4xl mx-auto bg-white rounded-3xl shadow-xl overflow-hidden border border-gray-100">
